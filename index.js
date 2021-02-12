3c28288edef7ef79f545fc0b3d80f00dfe434b4e
Eduard Benito <eduardo.benito.fernandez@ibm.com>
2021-02-12 13:33:27 +0100

    add connection to mongodb

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 const express = require('express')
 const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
 
 const app = express()
 const port = process.env.PORT || 3001
@@ -9,6 +10,13 @@ const port = process.env.PORT || 3001
 app.use(bodyParser.urlencoded({ extended: false}))
 app.use(bodyParser.json())
 
-app.listen(port, () => {
-  console.log(`GamesDB running in http://localhost:${port}`)
+mongoose.connect('mongodb+srv://admin:neOX3VedWiSNzuk1@gamescluster.cvd5b.mongodb.net/gamesdb?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true }, (err, res) => {
+  if (err) {
+    return console.log(`Failed to connect to data base: ${err}`)
+  }
+  console.log('Connection to data base... OK')
+
+  app.listen(port, () => {
+    console.log(`GamesDB running in http://localhost:${port}`)
+  })
 })
