b0f5cf0ecc9233550e13ffe7b42eec08724dd7d7
Eduard Benito <eduardo.benito.fernandez@ibm.com>
2021-02-12 13:21:19 +0100

    install body-parser and nodemon

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 'use strict'
 
 const express = require('express')
+const bodyParser = require('body-parser')
 
 const app = express()
 const port = process.env.PORT || 3001
+
+app.use(bodyParser.urlencoded({ extended: false}))
+app.use(bodyParser.json())
 
 app.listen(port, () => {
   console.log(`GamesDB running in http://localhost:${port}`)
